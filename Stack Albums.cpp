730cb8675c12fcd992b435f24d2ae97d4affd78a
Vidya Sinta Billkis <92220372+vidyasintabillkis@users.noreply.github.com>
2022-06-11 20:12:26 +0700

    full list album

--- Stack Albums.cpp
+++ Stack Albums.cpp
@@ -7,7 +7,12 @@ string album[]={"EXO - The War",
 		"Red Velvet - Bloom",
 		"NCT DREAM - Hot Sauce",
 		"EXO - Don't Mess Up My Tempo",
-	       }
+		"D.O  - Empathy The 1st Mini Album",
+		"NCT 127 - Sticker",
+		"Aespa - SAVAGE The 1st Mini Album", 
+		"NCT - Universe",
+		"ONEW - DICE The 2nd Mini Album",
+		"Baekhyun - BAMBI the 3rd Mini Album"}
 int harga[]={275000, 320000, 280000, 300000, }
 
 int main(){
