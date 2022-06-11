678eae69ed365e59bb785aaedbd00f3d5fcfcb6a
Nabillah Aisyah <92416388+nabillahaisyah@users.noreply.github.com>
2022-06-11 19:58:32 +0700

    Create Stack Albums.cpp

--- Stack Albums.cpp
+++ Stack Albums.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 string album[]={"EXO - The War", 
 		"Red Velvet - Bloom",
+		"NCT DREAM - Beatbox",
+		"EXO - Ko Ko Bop",
 	       }
-int harga[]={275000, 320000}
 
 int main(){
 	
