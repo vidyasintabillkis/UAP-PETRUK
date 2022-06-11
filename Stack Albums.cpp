ff85e5fdf1cb50dd6ec2cf19acd9c76d272aa8f3
Nabillah Aisyah <92416388+nabillahaisyah@users.noreply.github.com>
2022-06-11 20:00:51 +0700

    Update Stack Albums.cpp

--- Stack Albums.cpp
+++ Stack Albums.cpp
@@ -6,7 +6,7 @@ using namespace std;
 string album[]={"EXO - The War", 
 		"Red Velvet - Bloom",
 		"NCT DREAM - Beatbox",
-		"EXO - Ko Ko Bop",
+		"EXO - Don't Mess Up My Tempo",
 	       }
 
 int main(){
