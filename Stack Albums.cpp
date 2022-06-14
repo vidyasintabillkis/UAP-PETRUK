4a73abaf751e7a48ec057e9384b9f4f0754c3ab0
Osmycin Amelia <92617325+osmycin@users.noreply.github.com>
2022-06-14 17:37:58 +0700

    Update Stack Albums.cpp

--- Stack Albums.cpp
+++ Stack Albums.cpp
@@ -16,6 +16,8 @@ string album[]= {"EXO - The War",
 
 int harga[]= {275000, 320000, 289000, 312000, 278000, 315000, 273000, 326000, 265000, 284000};
 
+stack<int> tumpukan;
+int total = 0, input;
 
 void print(stack<int> x){
     stack<int> xy = x;
